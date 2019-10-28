a3a765ae0b1781378092535273f7ec4a397bb8c1
Joel Niedfeldt <joel@niedfeldt.com>
2019-10-28 18:56:29 -0300

    testing across balena and docker buildx for arm

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,10 @@ VOLUME /mnt/routes
 
 COPY . /src
 
-
+RUN [ "cross-build-start" ]
 RUN cd /src && ./build.sh "$(cat VERSION)"
 
 ONBUILD COPY ./build.sh /src/build.sh
 ONBUILD COPY ./modules.go /src/modules.go
 ONBUILD RUN cd /src && chmod +x ./build.sh && sleep 1 && sync && ./build.sh "$(cat VERSION)-custom"
+RUN [ "cross-build-end" ]
--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 set -e
-apk add --update go build-base git mercurial ca-certificates
+apk add --update go build-base git mercurial ca-certificates glide
 mkdir -p /go/src/github.com/gliderlabs
 cp -r /src /go/src/github.com/gliderlabs/logspout
 cd /go/src/github.com/gliderlabs/logspout
+echo "Architecture"
+uname -a 
 export GOPATH=/go
-go get github.com/Masterminds/glide && $GOPATH/bin/glide install
+go get github.com/Masterminds/glide
+echo "Installed glide package, now using to install dependencies..."
+$GOPATH/bin/glide install
+echo "Glide finished installing...let's build something"
 go build -ldflags "-X main.Version=$1" -o /bin/logspout
+echo "Done building!"
 apk del go git mercurial build-base
 rm -rf /go /var/cache/apk/* /root/.glide
 

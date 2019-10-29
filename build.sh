8a06fab3f0758b140497bba62698bd05bf67bd11
JN <joel@niedfeldt.com>
2019-10-29 21:35:04 +0000

    small changes to build on armv7

--- build.sh
+++ build.sh
@@ -10,7 +10,8 @@ export GOPATH=/go
 
 # go get github.com/Masterminds/glide
 # echo "Installed glide package, now using to install dependencies..."
-$GOPATH/bin/glide install
+#$GOPATH/bin/glide install
+glide install
 echo "Glide finished installing...let's build something"
 go build -ldflags "-X main.Version=$1" -o /bin/logspout
 echo "Done building!"
